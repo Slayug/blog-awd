67a456ad3e74df429fee602c7997e9b153e3f6f5
Slayug <bug.graph@gmail.com>
2017-04-30 17:55:39 +0300

    update root app js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-const INDEX_PATH = 'index.html';
+const INDEX_PATH = '/dist/index.html';
 
 app.use(express.static(__dirname + '/app'));
 app.listen(process.env.PORT || 3000 );
